f6bd67cbaef7b67dfc7751c464f443e7053dc446
Thomas Marques <marquesthom@gmail.com>
2013-03-11 14:35:53 +0100

    d
    Signed-off-by: Thomas Marques <marquesthom@gmail.com>

---
+++ Isima.InstantMessaging.WcfService/SessionService.svc.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Isima.InstantMessaging.WcfService
+{
+    // REMARQUE : vous pouvez utiliser la commande Renommer du menu Refactoriser pour changer le nom de classe "SessionService" à la fois dans le code, le fichier svc et le fichier de configuration.
+    // REMARQUE : pour lancer le client test WCF afin de tester ce service, sélectionnez SessionService.svc ou SessionService.svc.cs dans l'Explorateur de solutions et démarrez le débogage.
+    public class SessionService : ISessionService
+    {
+
+        private static TimeSpan expirationSpan = new TimeSpan(0, 1, 0);
+
+        private IList<Session> _sessions = new List<Session>();
+
+        public Session Register(DateTime current)
+        {
+            String name = "public";
+
+            if (!OperationContext.Current.ServiceSecurityContext.IsAnonymous)
+                name = Text.Sanitizer.SanitizeFileName(OperationContext.Current.ServiceSecurityContext.WindowsIdentity.Name);
+
+            Session session = new Session();
+            session.Identifiant = System.Guid.NewGuid();
+            session.WindowsIdentityName = name;
+            session.Expiration = current += expirationSpan;
+
+            _sessions.Add(session);
+
+            return session;
+        }
+
+        public bool GetPresence(String windowsIdentityName)
+        {
+            Session s = new Session();
+            s.WindowsIdentityName = windowsIdentityName;
+            return _sessions.Contains(s);
+        }
+    }
+}
